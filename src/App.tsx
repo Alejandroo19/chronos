33559dab56ff195cf5fb7991221d16fe3613f0e4
Alejandroo19 <alealejandrolidio@gmail.com>
2025-08-15 00:51:47 -0300

    fix: recriando branch main

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sunt,
+        incidunt dolor voluptate distinctio totam facere quod? Ea deserunt,
+        ullam quos a atque alias illo libero modi, voluptas, nam fugiat!
+      </p>
+    </>
+  );
+}
